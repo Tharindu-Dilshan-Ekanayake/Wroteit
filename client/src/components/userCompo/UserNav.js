f1916ad672c342e263f3f5d09b85c4ab63db8348
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-09-12 20:13:10 +0530

    Update UserNav.js

--- client/src/components/userCompo/UserNav.js
+++ client/src/components/userCompo/UserNav.js
@@ -8,22 +8,22 @@ import { IoSettings } from "react-icons/io5";
 export default function UserNav() {
   return (
     <div className='absolute flex justify-center transform -translate-x-1/2 bottom-8 left-1/2'>
-      <div className='flex justify-center border-2 rounded-full w-[600px] h-[70px] bg-opacity-10 bg-gradient-to-r from-[#19191ab6] to-[#27272ab2] text-white items-center shadow-2xl backdrop-blur-md'>
+      <div className='flex justify-center border-2 rounded-full w-[500px] h-[70px] bg-opacity-10 bg-gradient-to-b from-[#19191a] to-[#39393af6] text-white items-center shadow-2xl backdrop-blur-md'>
         <div className='flex justify-between w-screen'>
           <div className='pl-12'>
-            <GoHomeFill size={24} />
+            <GoHomeFill size={30} className='hover:text-orange-400 ' />
           </div>
           <div>
-            <FaUser size={24} />
+            <FaUser size={30} />
           </div>
           <div>
-            <IoNotifications size={24} />
+            <IoNotifications size={30} />
           </div>
           <div>
-            <FaPen size={24} />
+            <FaPen size={30} />
           </div>
           <div className='pr-12'>
-            <IoSettings size={24} />
+            <IoSettings size={30} />
           </div>
         </div>
       </div>
